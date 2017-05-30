11833df513f26f38ea7e4f310a99c8a010f97f6f
Guilherme Ogliari <guiiogliari@gmail.com>
2017-05-30 14:52:01 -0300

    1.2
    Manter o context

--- src/main/java/service/ServiceConversation.java
+++ src/main/java/service/ServiceConversation.java
@@ -1,5 +1,8 @@
 package service;
 
+import java.util.Map;
+
+import javax.ejb.Stateful;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,21 +13,28 @@ import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 
+@Stateful
 @Path("ServiceConversation")
 public class ServiceConversation {
+	
+	ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2017_02_03);
+	public static Map<String, Object> context = null;
+	
+	public ServiceConversation() {
+		service.setUsernameAndPassword("8733b87b-c43d-4d1a-b0ad-8bb035f50a91", "C1XroUw4jDJR");
+	}
 	
 	@POST
 	@GET
 	@Consumes("text/plain")
 	@Produces("application/json")
 	public String conversation(String message){
-				
-		ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2017_02_03);
-		service.setUsernameAndPassword("8733b87b-c43d-4d1a-b0ad-8bb035f50a91", "C1XroUw4jDJR");
-		
-		MessageRequest newMessage = new MessageRequest.Builder().inputText(message).build();
+						
+		MessageRequest newMessage = new MessageRequest.Builder().inputText(message).alternateIntents(true).context(context).build();
 		MessageResponse response = service.message("f7f9cdd7-e3f4-410c-b976-bbc895fc70fd", newMessage).execute();
 		
+		context = response.getContext();
+				
 		return response.toString();
 	}
 
